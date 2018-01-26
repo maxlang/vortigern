52810b8c56db818a5750358ea9820f3258df5af2
Max Lang <maxwell.g.lang@gmail.com>
2018-01-25 19:54:47 -0500

    add leaflet map

--- src/app/containers/About/index.tsx
+++ src/app/containers/About/index.tsx
@@ -1,14 +1,30 @@
 import * as React from 'react';
 const style = require('./style.css');
+// https://github.com/PaulLeCam/react-leaflet/issues/45
+import {Map, Marker, Popup, TileLayer } from 'react-leaflet-universal';
+
+console.log(style.blah);
 
 class About extends React.Component<any, any> {
   public render() {
+    const position = [51.505, -0.09];
     return (
       <div className={style.About}>
         <h4>About</h4>
+        <Map center={position} zoom={13} className={style.blah}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          />
+          <Marker position={position}>
+            <Popup>
+              <span>A pretty CSS3 popup.<br />Easily customizable.</span>
+            </Popup>
+          </Marker>
+        </Map>
       </div>
     );
   }
 }
 
-export { About }
+export {  About }
--- src/vendor/main.ts
+++ src/vendor/main.ts
@@ -4,3 +4,9 @@
  *   require('./bootstrap/css/bootstrap.min.css');
  * If you try using bootstrap 4, see: https://github.com/shakacode/bootstrap-loader/issues/244
  */
+
+ require('../../node_modules/leaflet/dist/leaflet.css');
+ require('../../node_modules/leaflet/dist/images/layers.png');
+ require('../../node_modules/leaflet/dist/images/marker-icon-2x.png');
+ require('../../node_modules/leaflet/dist/images/marker-icon.png');
+ require('../../node_modules/leaflet/dist/images/marker-shadow.png');
